78787f303d4b87e286f3e8b39ce176e5f2ad79dc
kodema5 <kodema5@outlook.com>
2022-01-07 15:29:32 -0800

    update auth_admin web_settings

--- src/auth_admin/web_settings_get.sql
+++ src/auth_admin/web_settings_get.sql
@@ -1,22 +1,35 @@
 create type auth_admin.web_settings_get_it as (
     _auth jsonb,
-    key text -- default '*'
+    keys text[], -- default '*'
+    namespace text,
+    user_id text
 );
 
 create function auth_admin.web_settings_get(req jsonb) returns jsonb as $$
 declare
     it auth_admin.web_settings_get_it = jsonb_populate_record(null::auth_admin.web_settings_get_it, auth_admin.auth(req));
     res jsonb;
-    keys_ text = coalesce(it.key, '*');
+    keys_ text[] = coalesce(it.keys, array['*']);
 begin
 
-    select jsonb_object_agg( s.key, to_jsonb(s))
-    into res
-    from auth_.setting s,
-        ( select unnest (string_to_array(keys_, ',')) ) as keys (k)
-    where s.key ~ (keys.k::lquery);
-
-    return jsonb_build_object('setting', res);
+    return jsonb_build_object(
+        'setting',
+        (select jsonb_object_agg(s.key, jsonb_strip_nulls(to_jsonb(s)))
+        from (
+            select a.key,
+                a.description,
+                coalesce(c.value, b.value, a.value) as value,
+                a.value as default_value,
+                b.value as namespace_value,
+                c.value as user_value
+            from (select unnest(array['*'])) as keys (k),
+            auth_.setting a
+            left outer join auth_.setting_namespace b on b.key = a.key
+                and b.ns_id = coalesce(it.namespace, (select ns_id from auth_.user where id=it.user_id))
+            left outer join auth_.setting_user c on c.key = a.key and c.user_id = it.user_id
+            where a.key ~ (keys.k::lquery)
+        ) s)
+    );
 end;
 $$ language plpgsql;
 
@@ -26,12 +39,13 @@ $$ language plpgsql;
     declare
         sid jsonb = tests.session_as_foo_admin();
         a jsonb;
+        t text;
     begin
         a = auth_admin.web_settings_get(sid);
         return next ok(a->'setting'->'test.a' is not null, 'returns settings by default');
 
         a = auth_admin.web_settings_get(sid || jsonb_build_object(
-            'key', 'test.a'
+            'keys', array['test.a']
         ));
         return next ok(a->'setting'->'test.a' is not null, 'returns specified key');
 
--- src/auth_admin/web_settings_put.sql
+++ src/auth_admin/web_settings_put.sql
@@ -1,45 +1,104 @@
+
+create function auth_admin.delete_setting(key_ ltree) returns void as $$
+    delete from auth_.setting where key = key_;
+$$ language sql;
+
+create function auth_admin.delete_setting_namespace(ns_id_ text, key_ ltree) returns void as $$
+    delete from auth_.setting_namespace where ns_id = ns_id_ and key = key_;
+$$ language sql;
+
+create function auth_admin.delete_setting_user(user_id_ text, key_ ltree) returns void as $$
+    delete from auth_.setting_user where user_id  = user_id_ and key = key_;
+$$ language sql;
+
+
+create function auth_admin.put_setting(key_ ltree, value_ jsonb, description_ text) returns void as $$
+    insert into auth_.setting (key, value, description)
+        values (key_, value_, description_)
+        on conflict (key)
+        do update set
+            value = coalesce(excluded.value, setting.value),
+            description = coalesce(excluded.description, setting.description);
+$$ language sql;
+
+create function auth_admin.put_setting_namespace(ns_id_ text, key_ ltree, value_ jsonb) returns void as $$
+    insert into auth_.setting_namespace (ns_id, key, value)
+        values (ns_id_, key_, value_)
+        on conflict (ns_id, key)
+        do update set
+            value = coalesce(excluded.value, setting_namespace.value);
+$$ language sql;
+
+create function auth_admin.put_setting_user (user_id_ text, key_ ltree, value_ jsonb) returns void as $$
+    insert into auth_.setting_user (user_id, key, value)
+        values (user_id_, key_, value_)
+        on conflict (user_id, key)
+        do update set
+            value = coalesce(excluded.value, setting_user.value);
+$$ language sql;
+
+
 create type auth_admin.web_settings_put_it as (
     _auth jsonb,
-    setting jsonb -- new/update: { key: {[key:new_key], [value:new_value], [description:new_description] }}
-                  -- delete    : { key: null }
+    setting jsonb,  -- new/update: { key: {[key:new_key], [value:new_value], [description:new_description] }}
+                    -- delete    : { key: null }
+    namespaces text[],
+    user_ids text[]
 );
 
 create function auth_admin.web_settings_put(req jsonb) returns jsonb as $$
 declare
     it auth_admin.web_settings_put_it = jsonb_populate_record(null::auth_admin.web_settings_put_it, auth_admin.auth(req));
     r record;
-    s auth_.setting;
     v auth_.setting;
+    is_ns boolean = it.namespaces is not null and cardinality(it.namespaces)>0;
+    is_usr boolean = it.user_ids is not null and cardinality(it.user_ids)>0;
+    is_sys boolean = not is_ns and not is_usr;
+    is_del boolean;
+    t text;
 begin
-   for r in
+    for r in
         select rs.key, rs.value
         from jsonb_each(it.setting) rs
     loop
-        if jsonb_typeof(r.value) = 'null' then
-            delete from auth_.setting where key = r.key::ltree;
-            continue;
+        is_del = jsonb_typeof(r.value) = 'null';
+        if not is_del then
+            if jsonb_typeof(r.value) = 'object' then
+                v = jsonb_populate_record(null::auth_.setting, r.value);
+            else
+                v.value = r.value;
+            end if;
+        end if;
+        v.key = r.key;
+
+        if is_sys then
+            if is_del then
+                perform auth_admin.delete_setting(v.key);
+            else
+                perform auth_admin.put_setting(v.key, v.value, v.description);
+            end if;
         end if;
 
-        v = jsonb_populate_record(null::auth_.setting, r.value);
+        if is_ns then
+            foreach t in array it.namespaces loop
+                if is_del then
+                    perform auth_admin.delete_setting_namespace(t, v.key);
+                else
+                    perform auth_admin.put_setting_namespace(t, v.key, v.value);
+                end if;
+            end loop;
+        end if;
 
-        select * into s from auth_.setting where key = r.key::ltree;
-        if not found then
-            insert into auth_.setting (key, value, description)
-            values (
-                r.key::ltree,
-                coalesce(v.value, s.value),
-                coalesce(v.description, s.description)
-            );
-            continue;
-        else
-            update auth_.setting set
-                value = coalesce(v.value, s.value),
-                description = coalesce(v.description, s.description)
-            where key = r.key::ltree;
-            continue;
+        if is_usr then
+            foreach t in array it.user_ids loop
+                if is_del then
+                    perform auth_admin.delete_setting_user(t, v.key);
+                else
+                    perform auth_admin.put_setting_user(t, v.key, v.value);
+                end if;
+            end loop;
         end if;
     end loop;
-
 
     return (select jsonb_build_object('setting', jsonb_object_agg( ss.key, to_jsonb(ss)))
     from auth_.setting ss);
@@ -53,25 +112,23 @@ $$ language plpgsql;
     create function tests.test_auth_admin_web_settings_put() returns setof text as $$
     declare
         sid jsonb = tests.session_as_foo_admin();
-        res jsonb;
+        a jsonb;
     begin
-        res = auth_admin.web_settings_get(sid || jsonb_build_object(
+        a = auth_admin.web_settings_get(sid || jsonb_build_object(
             'key', 'test.*'
         ));
-        return next ok(res->'setting'->'test.a' is not null, 'returns settings');
+        return next ok(a->'setting'->'test.a' is not null, 'returns settings');
 
-        -- add new
-        res = auth_admin.web_settings_put(sid || jsonb_build_object(
+        a = auth_admin.web_settings_put(sid || jsonb_build_object(
             'setting', jsonb_build_object(
                 'test.x', jsonb_build_object(
                     'value', 999, 'description', 'xxxx'
                 )
             )
         ));
-        return next ok(res->'setting'->'test.x'->>'description' = 'xxxx', 'creates new setting');
+        return next ok(a->'setting'->'test.x'->>'description' = 'xxxx', 'creates new setting');
 
-        -- update
-        res = auth_admin.web_settings_put(sid || jsonb_build_object(
+        a = auth_admin.web_settings_put(sid || jsonb_build_object(
             'setting', jsonb_build_object(
                 'test.x', jsonb_build_object(
                     'value', 1000,
@@ -79,15 +136,83 @@ $$ language plpgsql;
                 )
             )
         ));
-        return next ok(res->'setting'->'test.x'->>'description' = 'yyyy', 'updates existing setting');
+        return next ok(a->'setting'->'test.x'->>'description' = 'yyyy', 'updates existing setting');
 
-        -- delete
-        res = auth_admin.web_settings_put(sid || jsonb_build_object(
+        a = auth_admin.web_settings_put(sid || jsonb_build_object(
             'setting', jsonb_build_object(
                 'test.x', null
             )
         ));
-        return next ok(res->'setting'->'test.x' is null, 'deletes existing setting');
+        return next ok(a->'setting'->'test.x' is null, 'deletes existing setting');
     end;
     $$ language plpgsql;
+
+
+    create function tests.test_auth_admin_web_settings_put_namespace() returns setof text as $$
+    declare
+        sid jsonb = tests.session_as_foo_admin();
+        a jsonb;
+    begin
+        a = auth_admin.web_settings_get(sid || jsonb_build_object('namespace', 'dev'));
+        return next ok (a->'setting'->'test.a'->>'value' = '100', 'gets default value');
+
+        a = auth_admin.web_settings_put(sid || jsonb_build_object(
+            'namespaces', array['dev'],
+            'setting', jsonb_build_object('test.a', 200)
+        ));
+        a = auth_admin.web_settings_get(sid || jsonb_build_object('namespace', 'dev'));
+        return next ok (a->'setting'->'test.a'->>'value' = '200', 'able to set namespace value');
+
+        a = auth_admin.web_settings_put(sid || jsonb_build_object(
+            'namespaces', array['dev'],
+            'setting', jsonb_build_object('test.a', 300)
+        ));
+        a = auth_admin.web_settings_get(sid || jsonb_build_object('namespace', 'dev'));
+        return next ok (a->'setting'->'test.a'->>'value' = '300', 'able to update namespace value');
+
+        a = auth_admin.web_settings_put(sid || jsonb_build_object(
+            'namespaces', array['dev'],
+            'setting', jsonb_build_object('test.a', null)
+        ));
+        a = auth_admin.web_settings_get(sid || jsonb_build_object('namespace', 'dev'));
+        return next ok (a->'setting'->'test.a'->>'value' = '100', 'able to delete namespace value (got default back)');
+    end;
+    $$ language plpgsql;
+
+
+    create function tests.test_auth_admin_web_settings_put_user() returns setof text as $$
+    declare
+        sid jsonb = tests.session_as_foo_admin();
+        uid text = auth.get_user_id('dev', 'foo.user');
+        t text;
+        a jsonb;
+    begin
+        a = auth_admin.web_settings_get(sid || jsonb_build_object('user_id', uid));
+        return next ok (a->'setting'->'test.a'->>'value' = '100', 'gets default value');
+
+        a = auth_admin.web_settings_put(sid || jsonb_build_object(
+            'user_ids', array[uid],
+            'setting', jsonb_build_object('test.a', 200)
+        ));
+        a = auth_admin.web_settings_get(sid || jsonb_build_object('user_id', uid));
+        return next ok (a->'setting'->'test.a'->>'value' = '200', 'able to set user value');
+
+        a = auth_admin.web_settings_put(sid || jsonb_build_object(
+            'user_ids', array[uid],
+            'setting', jsonb_build_object('test.a', 300)
+        ));
+        a = auth_admin.web_settings_get(sid || jsonb_build_object('user_id', uid));
+        return next ok (a->'setting'->'test.a'->>'value' = '300', 'able to update user value');
+
+        a = auth_admin.web_settings_put(sid || jsonb_build_object(
+            'user_ids', array[uid],
+            'setting', jsonb_build_object('test.a', null)
+        ));
+        a = auth_admin.web_settings_get(sid || jsonb_build_object('user_id', uid));
+        return next ok (a->'setting'->'test.a'->>'value' = '100', 'able to delete user value');
+
+    end;
+    $$ language plpgsql;
+
+
 \endif
